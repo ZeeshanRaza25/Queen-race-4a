7f7c2a3bb500a87fe36080d092caf4ca33fb2c24
Zeeshan Raza <42145844+ZeeshanRaza25@users.noreply.github.com>
2020-08-13 01:45:29 +0500

    basic animation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,228 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useRef } from 'react';
 import './App.css';
+import useWebAnimations from '@wellyshen/use-web-animations';
 
 function App() {
+  // const ref1 = useRef(null);
+  // const ref2 = useRef(null);
+  // const ref3 = useRef(null);
+  // const ref3 = useRef(null);
+  // const ref4 = useRef(null);
+  const refBackground1 = useRef(null)
+  const refBackground2 = useRef(null)
+
+  const ref1 = useWebAnimations({
+    keyframes: [
+      { transform: 'translateY(0)' },
+      { transform: 'translateY(-100%)' },
+    ],
+    timing: {
+      duration: 600,
+      iterations: Infinity,
+      easing: 'steps(7, end)',
+    },
+    playbackRate: 1,
+  });
+
+  const ref2 = useWebAnimations({
+    keyframes: [
+      { transform: 'translateX(100%)' },
+      { transform: 'translateX(-100%)' },
+    ],
+    timing: {
+      duration: 12000,
+      iterations: Infinity,
+    },
+  });
+
+  // const { ref, playState } = useWebAnimations({
+  const ref3 = useWebAnimations({
+    keyframes: [
+      { transform: 'translate(100%)' },
+      { transform: 'translateX(-100%)' },
+    ],
+    timing: {
+      duration: 12000,
+      // fill: 'auto',
+      iterations: Infinity,
+      // direction: 'Infinity',
+      // easing: 'ease-in-out', // "steps(4,end)" "ease-in-out"
+    },
+  });
+
+  useEffect(() => {
+    function goFaster() {
+      ref3.playbackRate *= 1.1;
+      setInterval(() => {
+        if (ref3.playbackRate > 1) {
+          ref3.playbackRate *= 0.9;
+        }
+      }, 6000);
+    }
+    // function goFaster() {
+    //   ref1.playbackRate *= 1.1;
+    //   setInterval(() => {
+    //     if (ref1.playbackRate > 1) {
+    //       ref1.playbackRate *= 0.9;
+    //     }
+    //   }, 6000);
+    // }
+    window.addEventListener('click', goFaster);
+  });
+
+  // function goFaster() {
+  //   animateAliceQueenSprite.playbackRate *= 1.1;
+  //   setInterval(() => {
+  //     if (animateAliceQueenSprite.playbackRate > 1) {
+  //       animateAliceQueenSprite.playbackRate *= 0.9;
+  //     }
+  //   }, 6000);
+  // }
+  // window.addEventListener('click', goFaster);
+
+  // function goFaster() {
+  //   animateForeground2.playbackRate *= 1.1;
+  //   setInterval(() => {
+  //     if (animateForeground2.playbackRate > 1) {
+  //       animateForeground2.playbackRate *= 0.9;
+  //     }
+  //   }, 6000);
+  // }
+  // const ref3 = useWebAnimations({
+  //   keyframes: [
+  //     { transform: 'translateX(100%)' },
+  //     { transform: 'translateX(-100%)' },
+  //   ],
+  //   timing: {
+  //     duration: 36000,
+  //     iterations: Infinity,
+  //   },
+  // });
+  // useEffect(() => {
+  //   function goFaster() {
+  //     animateBackground2.playbackRate *= 1.1;
+  //     setInterval(() => {
+  //       if (animateBackground2.playbackRate > 1) {
+  //         animateBackground2.playbackRate *= 0.9;
+  //       }
+  //     }, 6000);
+  //   }
+  //   window.addEventListener('click', goFaster);
+  // });
+
+  // useEffect(() => {
+  //   const animateBackground1 =
+  //     ref4.current &&
+  //     ref4.current.animate(
+  //       [{ transform: 'translateX(100%)' }, { transform: 'translateX(-100%)' }],
+  //       {
+  //         duration: 36000,
+  //         iterations: Infinity,
+  //       }
+  //     );
+
+  //   function goFaster() {
+  //     animateBackground1.playbackRate *= 1.1;
+  //     setInterval(() => {
+  //       if (animateBackground1.playbackRate > 1) {
+  //         animateBackground1.playbackRate *= 0.9;
+  //       }
+  //     }, 6000);
+  //   }
+  //   window.addEventListener('click', goFaster);
+  // });
+
+  useEffect(() => {
+    const animateBackground1 = refBackground1.current && refBackground1.current.animate(
+      [{ transform: 'translateX(100%)' }, { transform: 'translateX(-100%)' }],
+      {
+        duration: 36000,
+        iterations: Infinity
+      }
+    )
+
+    function goFaster() {
+      animateBackground1.playbackRate *= 1.1;
+      setInterval(
+        () => {
+          if (animateBackground1.playbackRate > 1) {
+            animateBackground1.playbackRate *= .9;
+          }
+        }, 6000)
+    }
+    window.addEventListener('click', goFaster)
+  })
+
+  useEffect(() => {
+    const animateBackground2 = refBackground2.current && refBackground2.current.animate(
+      [{ transform: 'translateX(100%)' }, { transform: 'translateX(-100%)' }],
+      {
+        duration: 36000,
+        iterations: Infinity
+      }
+    )
+    function goFaster() {
+      animateBackground2.playbackRate *= 1.1;
+      setInterval(
+        () => {
+          if (animateBackground2.playbackRate > 1) {
+            animateBackground2.playbackRate *= .9;
+          }
+        }, 6000)
+    }
+    window.addEventListener('click', goFaster)
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="wrapper">
+        <div className="sky"></div>
+        <div className="earth">
+          <div id="red-queen_and_alice">
+            <img
+              ref={ref1.ref}
+              id="red-queen_and_alice_sprite"
+              src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/sprite_running-alice-queen_small.png"
+              srcSet="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/sprite_running-alice-queen.png 2x"
+              alt="Alice and the Red Queen running to stay in place."
+            />
+          </div>
+        </div>
+        <div className="scenery" id="foreground1" ref={ref2.ref}>
+          <img
+            id="palm3"
+            src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/palm3_small.png"
+            srcSet="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/palm3.png 2x"
+            alt=" "
+          />
+        </div>
+        <div className="scenery" id="foreground2" ref={ref3.ref}>
+          <img
+            id="bush"
+            src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/bush_small.png"
+            srcSet="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/bush.png 2x"
+            alt=" "
+          />
+          <img
+            id="w_rook_upright"
+            src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/w_rook_upright_small.png"
+            srcSet="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/w_rook_upright.png 2x"
+            alt=" "
+          />
+        </div>
+        <div ref={refBackground1} className="scenery" id="background2">
+          <img id="r_pawn" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/r_pawn_small.png" srcSet="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/r_pawn.png 2x" alt=" " />
+
+          <img id="r_knight" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/r_knight_small.png" srcSet="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/r_knight.png 2x" alt=" " />
+          <img id="palm2" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/palm2_small.png" srcSet="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/palm2.png 2x" alt=" " />
+        </div>
+        <div ref={refBackground2} className="scenery" id="Background2">
+          <img id="r_pawn_upright" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/r_pawn_upright_small.png" srcSet="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/r_pawn_upright.png 2x" alt=" " />
+          <img id="w_rook" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/w_rook_small.png" srcSet="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/w_rook.png 2x" alt=" " />
+          <img id="palm1" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/palm1_small.png" srcSet="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/palm1.png 2x" alt=" " />
+        </div>
+      </div>
+    </>
   );
 }
 
